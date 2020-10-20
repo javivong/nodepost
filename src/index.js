007acf3ae161a2fd400f9837cf92dda5c262bc60
javivong <57103318+javivong@users.noreply.github.com>
2020-10-20 19:29:43 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+let jugador = {
+    nombre:'',
+    apellido: '',
+    score:''
+   };
+
+let respuesta = {
+    error: false,
+    codigo: 200,
+    mensaje: ''
+};
+
+app.get('/', function (req, res) {
+    respuesta = {
+        error: true,
+        codigo: 200,
+        mensaje: 'Punto de inicio'
+       };
+    res.send(respuesta);
+});
+
+app.get('/hola', function (req, res) {
+    res.send('[GET]Saludos desde express');
+});
+
+app.listen(3000, () => {
+ console.log("El servidor está inicializado en el puerto 3000");
+});
