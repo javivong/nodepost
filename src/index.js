afe9f084465ad9a2ce56286a91336a7fe385c8d3
javivong <a19javdedgar@inspedralbes.cat>
2020-10-21 07:59:01 +0200

    almost end

--- src/index.js
+++ src/index.js
@@ -30,6 +30,33 @@ app.get('/hola', function (req, res) {
     res.send('[GET]Saludos desde express');
 });
 
+app.post('/gamer', function (req, res) {
+    if (req.body.nombre == null || req.body.apellido == null || req.body.score == null) {
+        respuesta = {
+            error: true,
+            codigo: 502,
+            mensaje: 'Los campos nombre, apellido y score son obligatorios' 
+        };
+    }
+    else if (Object.keys(req.body).every(i => req.body[i] == jugador[i])) {
+        respuesta = {
+            error: true,
+            codigo: 503,
+            mensaje: 'Este jugador ya ha sido creado' 
+        };
+    }
+    else {
+        jugador = req.body;
+        respuesta = {
+            error: false,
+            codigo: 200,
+            mensaje: 'Jugador creado',
+            respuesta: jugador 
+        };
+    };
+    res.send(respuesta);
+})
+
 app.listen(3000, () => {
  console.log("El servidor está inicializado en el puerto 3000");
 });
