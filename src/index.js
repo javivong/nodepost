6043ffe03ad83e97709b52e7cc6267bb1cdbd4e4
javivong <a19javdedgar@inspedralbes.cat>
2020-11-14 17:06:29 +0100

    Prepare to heroku

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ const express = require("express"),
       api = require("./api"),
       swaggerUi = require('swagger-ui-express'),
       swaggerDocument = require('./openapi.json'),
-      app = express();
+      app = express(),
+      router = express.Router();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-api.set(app);
+app.use('/api/v1', router);
+api.set(router);
+
 
 app.listen(3000, () => {
     console.log("El servidor está inicializado en el puerto 3000");
